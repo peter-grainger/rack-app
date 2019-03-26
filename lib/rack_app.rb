e6a126c82d56f903a3dd84d02d6f1c0cd7414c98
Victor Martins <victor.martins@sage.com>
2019-03-26 23:14:34 +0000

    Render ERB View

--- lib/rack_app.rb
+++ lib/rack_app.rb
@@ -1,5 +1,21 @@
+require 'erb'
+
 class RackApp
   def call(env)
-    [200, {}, ["Hello !!!!xxx"]]
+    Rack::Response.new(
+      render(
+        view_path("index.html.erb")
+      )
+    )
+  end
+
+  private
+
+  def render(path)
+    ERB.new(File.read(path)).result(binding)
+  end
+
+  def view_path(template)
+    File.expand_path("../../views/#{template}", __FILE__)
   end
 end
